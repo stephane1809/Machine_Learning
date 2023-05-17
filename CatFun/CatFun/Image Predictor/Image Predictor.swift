7dce7324a52608eef605103bd5b091ea450677b8
Stephane <stephanegiraolinhares@gmail.com>
2023-05-17 10:26:24 -0300

    nada ainda

---
+++ CatFun/CatFun/Image Predictor/Image Predictor.swift
@@ -0,0 +1,36 @@
+//
+//  Image Predictor.swift
+//  CatFun
+//
+//  Created by Stephane Girão Linhares on 16/05/23.
+//
+
+import Foundation
+import CoreML
+import Vision
+
+
+class ImagePredictor {
+    static func createImageClassifier() -> VNCoreMLModel {
+        // Use a default model configuration.
+        let defaultConfig = MLModelConfiguration()
+
+        // Create an instance of the image classifier's wrapper class.
+        let imageClassifierWrapper = try? CatClassifier(configuration: defaultConfig)
+
+        guard let imageClassifier = imageClassifierWrapper else {
+            fatalError("App failed to create an image classifier model instance.")
+        }
+
+        // Get the underlying model instance.
+        let imageClassifierModel = imageClassifier.model
+
+        // Create a Vision instance using the image classifier's model instance.
+        guard let imageClassifierVisionModel = try? VNCoreMLModel(for: imageClassifierModel) else {
+            fatalError("App failed to create a `VNCoreMLModel` instance.")
+        }
+
+        return imageClassifierVisionModel
+    }
+}
+
